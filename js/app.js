bb3ec745f93e72ac5227fe3040d436a39cd7adc2
FrankieBoye <frankie4@btinternet.com>
2019-09-27 11:07:18 +0100

    installed live-server and created function to view data

--- js/app.js
+++ js/app.js
@@ -4,3 +4,24 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = "Iv1.e1fcdec707a6e340";
+const client_secret = "930612375a150aa3d890fbc6a7341ffa1c63feb2";
+
+const fetchUsers = async (user) => {
+  const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+
+const data = await api_call.json();
+return { data }
+};
+
+const showData = () => {
+  fetchUsers(inputValue.value).then((res) => {
+    console.log(res);
+  })
+}
+
+
+searchButton.addEventListener("click", () => {
+  showData();
+})
